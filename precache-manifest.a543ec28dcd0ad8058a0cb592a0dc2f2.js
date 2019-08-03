98301e097463f5e254bb7d20794ca1f12af69054
Steven J <3893631+AlmostInteractive@users.noreply.github.com>
2019-08-03 12:51:51 -0500

    Updates

---
+++ precache-manifest.a543ec28dcd0ad8058a0cb592a0dc2f2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcbd231abd25c96cb785500b61604e42",
+    "url": "/tapatio-poker-react/index.html"
+  },
+  {
+    "revision": "3390215c3f1f4ebda48f",
+    "url": "/tapatio-poker-react/static/css/main.47630eb5.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/tapatio-poker-react/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "3390215c3f1f4ebda48f",
+    "url": "/tapatio-poker-react/static/js/main.1266440f.chunk.js"
+  },
+  {
+    "revision": "328630f43ddaa5442724",
+    "url": "/tapatio-poker-react/static/js/runtime~main.61cf3600.js"
+  }
+]);
